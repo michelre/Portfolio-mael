056188ae54c7a6f3542a2d4fb96ef6056158c356
Rémi Michel <remi.michel38@gmail.com>
2025-01-10 18:35:48 +0100

    feat(): add PDF + lien + emailjs with env file

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import './style.css';
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser'
+
+const {VITE_EMAILJS_SECRET, VITE_EMAILJS_SERVICEID, VITE_EMAILJS_TEMPLATEID} = import.meta.env
 
 document.addEventListener('DOMContentLoaded', function() {
-  emailjs.init("");
+  emailjs.init(VITE_EMAILJS_SECRET);
   document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     // Ces valeurs doivent correspondre à votre modèle EmailJS
-    emailjs.sendForm('', '', this)
+    emailjs.sendForm(VITE_EMAILJS_SERVICEID, VITE_EMAILJS_TEMPLATEID, this)
       .then(function() {
         alert('Message envoyé avec succès !');
       }, function(error) {
